ed3dd0d1dc677804b518469810c40f5ef02bab8e
Stefan <stefan.grochowski@gmx.de>
2023-03-09 10:59:55 +0100

    Changed scale_colour_manual for scale_fill_manual

--- R/plot_themes.R
+++ R/plot_themes.R
@@ -458,7 +458,7 @@ theme_tueb_2 <- function(col_title = uni_tuebingen_2[[1]], # "blue"
         # background:  
         plot.background = ggplot2::element_rect(fill = "transparent", color = NA),
         complete = TRUE), 
-    scale_color_manual(values=usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
+    scale_fill_manual(values=usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
                                        uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
                                        uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
                                        uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
