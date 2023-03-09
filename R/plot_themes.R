7c4a9ab10c11edc5633ea287f1600c8469b98491
Stefan <stefan.grochowski@gmx.de>
2023-03-09 13:21:29 +0100

    Testing theme 2

--- R/plot_themes.R
+++ R/plot_themes.R
@@ -467,17 +467,17 @@ theme_tueb_2 <- function(binary = TRUE,
         plot.background = ggplot2::element_rect(fill = "transparent", color = NA),
         complete = TRUE), 
     
-    if (binary==TRUE) {
-      scale_fill_manual(values=usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
-                                        uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
-                                        uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
-                                        uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
-    } else {
-      scale_color_gradientn(colors = usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
-                                              uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
-                                              uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
-                                              uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
-    }
+      #if (binary==TRUE) {
+        scale_fill_manual(values=usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
+                                          uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
+                                          uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
+                                          uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
+      # } else {
+      #   scale_color_gradientn(colors = usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
+      #                                           uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
+      #                                           uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
+      #                                           uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
+      # }
     
     
   )
