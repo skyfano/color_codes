0eba851eeaa74264e8963cd76536ae5cbf655371
Stefan <stefan.grochowski@gmx.de>
2023-04-04 13:39:46 +0200

    changed second theme for tuebignen

--- R/plot_themes.R
+++ R/plot_themes.R
@@ -420,7 +420,7 @@ theme_tueb_1 <- function(binary = TRUE,
 
 # - Definition: ---- 
 
-theme_tueb_2 <- function(#binary = TRUE, 
+theme_tueb_2 <- function(binary = TRUE, 
                          col_title = uni_tuebingen_2[[1]], # "blue"  
                          base_size = 11, 
                          base_family = "", 
@@ -467,17 +467,17 @@ theme_tueb_2 <- function(#binary = TRUE,
         plot.background = ggplot2::element_rect(fill = "transparent", color = NA),
         complete = TRUE), 
     
-      #if (binary==TRUE) {
+      if (binary==TRUE) {
         scale_fill_manual(values=usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
                                           uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
                                           uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
                                           uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
-      # } else {
-      #   scale_color_gradientn(colors = usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
-      #                                           uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
-      #                                           uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
-      #                                           uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
-      # }
+       } else {
+         scale_color_gradientn(colors = usecol(c(uni_tuebingen_2[1], uni_tuebingen_2[2], uni_tuebingen_2[3],
+                                                 uni_tuebingen_2[4], uni_tuebingen_2[5], uni_tuebingen_2[6],
+                                                 uni_tuebingen_2[7], uni_tuebingen_2[8], uni_tuebingen_2[9],
+                                                 uni_tuebingen_2[10], uni_tuebingen_2[11], uni_tuebingen_2[12])))
+       }
     
     
   )
